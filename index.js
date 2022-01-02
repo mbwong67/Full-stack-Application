b4694b2cddb72dd1a68758a7a4f643ec5b575fc7
Eleni Maria <egoranites@hotmail.com>
2022-01-02 13:49:52 -0500

    Update index.js
    revert to include ", false"

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const methodOverride = require('method-override');
 const parser = require('body-parser');
 //find and update (creates new not update)
 const mongoose = require('mongoose');
-mongoose.set('useFindAndUpdate');
+mongoose.set('useFindAndUpdate', false);
 //setup handlebars(keep above controller)
 // const hbs = require('hbs');
 app.set('view engine', 'hbs');
